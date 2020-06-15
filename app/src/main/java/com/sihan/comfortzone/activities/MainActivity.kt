c3a866edd120cbcb19711d6edcc690d1597a23fb
sihan-tawsik <sihantawsik@gmail.com>
2020-06-15 21:52:25 +0600

    fixed main activity back button bug

--- app/src/main/java/com/sihan/comfortzone/activities/MainActivity.kt
+++ app/src/main/java/com/sihan/comfortzone/activities/MainActivity.kt
@@ -3,10 +3,15 @@ package com.sihan.comfortzone.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sihan.comfortzone.R
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        finishAffinity()
     }
 }
