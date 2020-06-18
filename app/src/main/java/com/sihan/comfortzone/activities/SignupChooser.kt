9fed38d96d50062d9af76a00131ceb9de23c4f05
sihan-tawsik <sihantawsik@gmail.com>
2020-06-18 19:25:19 +0600

    added facebook sign in dependencies

--- app/src/main/java/com/sihan/comfortzone/activities/SignupChooser.kt
+++ app/src/main/java/com/sihan/comfortzone/activities/SignupChooser.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.facebook.CallbackManager
 import com.sihan.comfortzone.R
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
 
 class SignupChooser : AppCompatActivity() {
     private lateinit var email_sign_up: Button
+    private lateinit var callbackManager: CallbackManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_chooser)
