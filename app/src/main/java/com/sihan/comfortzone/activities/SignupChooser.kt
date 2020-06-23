d57072ff8849212f60d5758fb5137e79f851f1f7
sihan-tawsik <sihantawsik@gmail.com>
2020-06-23 21:54:25 +0600

    implemented facebook login

--- app/src/main/java/com/sihan/comfortzone/activities/SignupChooser.kt
+++ app/src/main/java/com/sihan/comfortzone/activities/SignupChooser.kt
@@ -1,16 +1,25 @@
 package com.sihan.comfortzone.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.AccessToken
 import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.sihan.comfortzone.R
-import com.facebook.FacebookSdk
-import com.facebook.appevents.AppEventsLogger
 
 class SignupChooser : AppCompatActivity() {
     private lateinit var email_sign_up: Button
+    private lateinit var facebook_login_button: LoginButton
     private lateinit var callbackManager: CallbackManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +37,46 @@ class SignupChooser : AppCompatActivity() {
 
     private fun bindWidgets() {
         email_sign_up = findViewById(R.id.email_sign_up)
+        facebook_login_button = findViewById(R.id.facebook_login_button)
+        facebook_login_button.setPermissions("email", "public_profile")
+        callbackManager = CallbackManager.Factory.create()
+        facebook_login_button.registerCallback(
+            callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult?) {
+                    handleAccessToken(result?.accessToken)
+                }
+
+                override fun onCancel() {
+                    Log.e("result", "failed")
+                }
+
+                override fun onError(error: FacebookException?) {
+                    Log.e("main", error.toString())
+                }
+
+            })
+    }
+
+    private fun handleAccessToken(accessToken: AccessToken?) {
+        val authCredential = FacebookAuthProvider.getCredential(accessToken!!.token)
+        val firebaseAuth = Firebase.auth
+        firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(
+            this
+        ) { p0 ->
+            if (p0.isSuccessful) {
+                startActivity(Intent(this@SignupChooser, MainActivity::class.java))
+            } else {
+                Toast.makeText(this@SignupChooser, "failed", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data)
     }
 
     override fun onBackPressed() {
