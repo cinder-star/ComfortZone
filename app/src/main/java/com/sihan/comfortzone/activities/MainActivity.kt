5969729fe80b0a22a8480089088e725ba68bf0c4
sihan-tawsik <sihantawsik@gmail.com>
2020-06-28 12:56:11 +0600

    reverted searchbar changes

--- app/src/main/java/com/sihan/comfortzone/activities/MainActivity.kt
+++ app/src/main/java/com/sihan/comfortzone/activities/MainActivity.kt
@@ -3,12 +3,10 @@ package com.sihan.comfortzone.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ismaeldivita.chipnavigation.ChipNavigationBar
-import com.miguelcatalan.materialsearchview.MaterialSearchView
 import com.sihan.comfortzone.R
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navigationBar: ChipNavigationBar
-    private lateinit var materialSearchView: MaterialSearchView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,24 +15,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun bindListeners() {
-        materialSearchView.setOnQueryTextListener(object: MaterialSearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                TODO("Not yet implemented")
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                TODO("Not yet implemented")
-            }
-
-        })
     }
 
     private fun bindWidgets() {
         navigationBar = findViewById(R.id.nav_bar)
         navigationBar.setItemSelected(0)
-        materialSearchView = findViewById(R.id.my_search_bar)
-        materialSearchView.closeSearch()
     }
+
 
     override fun onBackPressed() {
         finishAffinity()
