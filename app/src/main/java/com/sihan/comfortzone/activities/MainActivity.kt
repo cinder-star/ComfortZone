4922cbc85c0f5c893c2ef9259247673856d33659
Samnan <namanush.rsr.16@gmail.com>
2020-06-28 19:43:05 +0600

    Implement NavDrawer toggle

--- app/src/main/java/com/sihan/comfortzone/activities/MainActivity.kt
+++ app/src/main/java/com/sihan/comfortzone/activities/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.sihan.comfortzone.activities
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.ismaeldivita.chipnavigation.ChipNavigationBar
 import com.sihan.comfortzone.R
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navigationBar: ChipNavigationBar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var toolbar: Toolbar
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,7 +28,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun bindWidgets() {
         navigationBar = findViewById(R.id.nav_bar)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_drawer)
+        toolbar = findViewById(R.id.toolbar)
+
         navigationBar.setItemSelected(R.id.home)
+        setSupportActionBar(toolbar)
+        actionBarDrawerToggle =
+            ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.open_nav,
+                R.string.close_nav
+            )
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
     }
 
 
