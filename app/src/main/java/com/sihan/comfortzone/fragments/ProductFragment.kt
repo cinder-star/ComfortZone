1c89fc89174c0fed9ef02e68abaf7338cf6ddb10
Samnan <namanush.rsr.16@gmail.com>
2020-07-02 17:14:36 +0600

    Update UI for ProductFragment

--- app/src/main/java/com/sihan/comfortzone/fragments/ProductFragment.kt
+++ app/src/main/java/com/sihan/comfortzone/fragments/ProductFragment.kt
@@ -1,13 +1,11 @@
 package com.sihan.comfortzone.fragments
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
@@ -37,7 +35,8 @@ class ProductFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_product, container, false)
         swipeRefreshLayout = view.findViewById(R.id.swipe_bar)
         productRecyclerView = view.findViewById(R.id.product_list)
-        productRecyclerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        productRecyclerView.layoutManager =
+            StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
         activity?.let { ContextCompat.getColor(it, R.color.brand) }?.let {
             swipeRefreshLayout.setColorSchemeColors(
                 it
