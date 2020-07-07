d6581527a5692c68d953c234e5657bb0a1386e50
sihan-tawsik <sihantawsik@gmail.com>
2020-07-08 00:13:35 +0600

    changed on backpressed for main activity

--- app/src/main/java/com/sihan/comfortzone/activities/MainActivity.kt
+++ app/src/main/java/com/sihan/comfortzone/activities/MainActivity.kt
@@ -138,7 +138,7 @@ class MainActivity : AppCompatActivity(){
         if (materialSearchView.isSearchOpen) {
             materialSearchView.closeSearch()
         } else {
-            finishAffinity()
+            super.onBackPressed()
         }
     }
 
---
+++ app/src/main/java/com/sihan/comfortzone/domains/Category.kt
@@ -0,0 +1,6 @@
+package com.sihan.comfortzone.domains
+
+data class Category (
+    val name: String? = null,
+    val imagePath: String? = null
+)
