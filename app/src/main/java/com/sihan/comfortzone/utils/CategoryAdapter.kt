9a5c205f725b78085b6549ca6cc43b67b3839362
sihan-tawsik <sihantawsik@gmail.com>
2020-07-08 18:25:49 +0600

    updated category images

--- app/src/main/java/com/sihan/comfortzone/utils/CategoryAdapter.kt
+++ app/src/main/java/com/sihan/comfortzone/utils/CategoryAdapter.kt
@@ -1,12 +1,14 @@
 package com.sihan.comfortzone.utils
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.sihan.comfortzone.R
 import com.sihan.comfortzone.domains.Category
 import com.sihan.comfortzone.repositories.MyAdapter
@@ -23,7 +25,7 @@ class CategoryAdapter(var context: Context, private var categories: List<Categor
     override fun getItemCount(): Int = categories.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bindItem(categories[position])
+        holder.bindItem(categories[position], context)
     }
 
     class ViewHolder(
@@ -31,9 +33,14 @@ class CategoryAdapter(var context: Context, private var categories: List<Categor
         private var onCategoryListener: OnCategoryListener,
         private var categories: List<Category>
     ) : RecyclerView.ViewHolder(view), View.OnClickListener {
-        fun bindItem(category: Category) {
+        fun bindItem(category: Category, context: Context) {
+            val imageView: ImageView = itemView.findViewById(R.id.category_image)
+            val imageRef = Firebase.storage.reference.child("categories/"+category.imagePath)
             itemView.findViewById<TextView>(R.id.category_name).text = category.name
             itemView.setOnClickListener(this)
+            GlideApp.with(context)
+                .load(imageRef)
+                .into(imageView)
         }
 
         override fun onClick(p0: View?) {
