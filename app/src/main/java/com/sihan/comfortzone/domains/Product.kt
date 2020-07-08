f912bd64b32f4bbfe1f8f6b77c7b9c62496861ac
sihan-tawsik <sihantawsik@gmail.com>
2020-07-09 01:01:11 +0600

    updated product data path

--- app/src/main/java/com/sihan/comfortzone/domains/Product.kt
+++ app/src/main/java/com/sihan/comfortzone/domains/Product.kt
@@ -6,5 +6,8 @@ data class Product (
     var id: String? = null,
     var name: String? = null,
     var price: Int? = 0,
-    var imagePath: String? = null
+    var prevPrice: Int? = 0,
+    var imagePath: String? = null,
+    var categoryName: String? = null,
+    var popular: String? = "no"
 ): Serializable
